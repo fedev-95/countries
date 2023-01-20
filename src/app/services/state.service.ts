d1319f3cebdbd4c26839d0ff8e213655a3329860
Federico Burgos <federicoburgos.dev@gmail.com>
2023-01-20 02:30:06 -0300

    simplificado de codigo

--- src/app/services/state.service.ts
+++ src/app/services/state.service.ts
@@ -5,19 +5,22 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class StateService {
-
-  darkMode = new BehaviorSubject<string>(localStorage.getItem('darkMode') || 'false');
+  
+  darkMode = new BehaviorSubject<boolean>(localStorage.getItem('darkMode') === 'true'? true : false);
   darkMode$ = this.darkMode.asObservable();
 
-  toggleTheme(req?: boolean) {
-    if (!localStorage.getItem('darkMode') || localStorage.getItem('darkMode') === 'false') {
-      localStorage.setItem('darkMode', 'true');
-      this.darkMode.next('true');
-    } else if (localStorage.getItem('darkMode') === 'true' && req) {
+  toggleTheme(setFalse?: boolean) {
+
+    let localSetting: string | null = localStorage.getItem('darkMode');
+
+    if (localSetting === 'true' && setFalse) {
       localStorage.setItem('darkMode', 'false');
-      this.darkMode.next('false');
+      this.darkMode.next(false);
+    } else if (!localSetting || localSetting === 'false') {
+      localStorage.setItem('darkMode', 'true');
+      this.darkMode.next(true);
     }
     document.body.classList.toggle('dark-theme');
   }
-  
+
 }
