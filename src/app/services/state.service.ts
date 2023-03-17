581238dcc71e05f95391cb4e30de03ef972ae376
Federico <federicoburgos.dev@gmail.com>
2023-03-17 05:30:06 -0300

    dark mode

--- src/app/services/state.service.ts
+++ src/app/services/state.service.ts
@@ -9,7 +9,7 @@ export class StateService {
   darkMode = new BehaviorSubject<boolean>(localStorage.getItem('darkMode') === 'true'? true : false);
   darkMode$ = this.darkMode.asObservable();
 
-  toggleTheme(setFalse?: boolean) {
+  toggleTheme(setLightMode?: boolean) {
 
     let localSetting: string | null = localStorage.getItem('darkMode');
 
@@ -18,7 +18,7 @@ export class StateService {
       this.darkMode.next(true);
     }
 
-    if (localSetting === 'true' && setFalse) {
+    if (localSetting === 'true' && setLightMode) {
       localStorage.setItem('darkMode', 'false');
       this.darkMode.next(false);
     } else if (!localSetting || localSetting === 'false') {
